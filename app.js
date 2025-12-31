38f2d66dab932b6b1914de6f593de6c380999ce7
natthakitkich <natthakit.kich@gmail.com>
2025-12-31 23:09:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+const pages = document.querySelectorAll(".page");
+const nextBtn = document.getElementById("nextBtn");
 const startBtn = document.getElementById("startBtn");
-const landing = document.querySelector(".landing");
-const content = document.getElementById("content");
 const music = document.getElementById("bgMusic");
 
-startBtn.addEventListener("click", () => {
-  landing.style.display = "none";
-  content.classList.remove("hidden");
+let current = 0;
 
+// iOS-safe play
+function playMusic() {
   music.currentTime = 0;
   music.volume = 0.3;
   music.play();
+}
+
+// show page
+function showPage(index) {
+  pages.forEach(p => p.classList.remove("active"));
+  pages[index].classList.add("active");
+}
+
+startBtn.addEventListener("click", () => {
+  playMusic();
+  current = 1;
+  showPage(current);
+});
+
+nextBtn.addEventListener("click", () => {
+  if (current < pages.length - 1) {
+    current++;
+    showPage(current);
+  }
 });
